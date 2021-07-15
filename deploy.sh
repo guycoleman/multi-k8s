e2f5c6c495be0445b743b0cdb6312aae83a61494
Guy Coleman <gtc+github@mm.st>
2021-07-15 20:25:39 +0100

    Fix server image labelling

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker build -t guytcoleman/multi-client:latest -t guytcoleman/multi-client:$GIT_SHA ./client
-docker build -t guytcoleman/multi-client:latest -t guytcoleman/multi-client:$GIT_SHA ./server
+docker build -t guytcoleman/multi-server:latest -t guytcoleman/multi-server:$GIT_SHA ./server
 docker build -t guytcoleman/multi-worker:latest -t guytcoleman/multi-worker:$GIT_SHA ./worker
 
 docker push guytcoleman/multi-client:latest
